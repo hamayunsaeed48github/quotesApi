51d9589fae22d2b5903918c7c1c657f9350b376e
Hamayun Saeed <hamayunsaeed48@gmail.com>
2024-09-15 08:38:37 -0700

    vercel update

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -59,7 +59,28 @@ const deleteProduct = async (req,res) =>{
     } catch (error) {
         res.status(500).json({message:error.message})
     }
-}
+};
+
+const UpdateProductById = async (req,res) =>{
+    try {
+ 
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id,req.body)
+    
+        if(!product){
+            return res.status(404).json({message:"product not found"})
+        }
+    
+        const UpdateProduct = await Product.findById(id)
+        res.status(200).json(UpdateProduct)
+        
+       } catch (error) {
+        res.status(500).json({message:error.message})
+       }
+
+
+};
+
 
 
 module.exports={
@@ -67,5 +88,6 @@ module.exports={
     getProductById,
     addProduct,
     deleteProduct,
-    deleteProductById
+    deleteProductById,
+    UpdateProductById
 }
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -5,7 +5,9 @@ const {getProducts,
     getProductById,
     addProduct,
     deleteProduct,
-    deleteProductById
+    deleteProductById,
+    UpdateProductById
+
 }= require("../controller/product.controller");
 
 router.get("/quotes",getProducts);
@@ -13,5 +15,6 @@ router.get("/:id",getProductById);
 router.post("/add/quotes",addProduct);
 router.delete("/delete/all/quotes",deleteProduct);
 router.delete("/delete/:id",deleteProductById);
+router.put("/update/:id",UpdateProductById);
 
 module.exports = router;
